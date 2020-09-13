9edf447ec78ff6018934a8392cc1463e78249b59
Quentin Blondel <qb@volpy.com>
2020-09-13 14:18:31 +0200

    3. Service Worker - résultat

--- main.js
+++ main.js
@@ -17,3 +17,9 @@ function loadTechnologies(technos) {
 }
 
 loadTechnologies(technos);
+
+if(navigator.serviceWorker) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .catch(err => console.error('service worker NON enregistré', err));
+}
