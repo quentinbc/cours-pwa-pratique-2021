397eec0092ef7e061fa909a5e54e34ce727d50f4
Quentin Blondel <qb@volpy.com>
2020-09-19 16:34:53 +0200

    8. Notifications Push - résultat

--- main.js
+++ main.js
@@ -26,11 +26,11 @@ function loadTechnologies(technos) {
 
 loadTechnologies(technos);
 
-if(navigator.serviceWorker) {
-    navigator.serviceWorker
-        .register('sw.js')
-        .catch(err => console.error('service worker NON enregistré', err));
-}
+// if(navigator.serviceWorker) {
+//     navigator.serviceWorker
+//         .register('sw.js')
+//         .catch(err => console.error('service worker NON enregistré', err));
+// }
 
 
 //7.1 Notifications non persistantes
@@ -57,3 +57,82 @@ if(navigator.serviceWorker) {
 //         }
 //     })
 // }
+
+// 3.2
+if(navigator.serviceWorker) {
+	// Enregistrement du service worker
+    navigator.serviceWorker
+        .register('sw.js')
+        
+        // 8.4 Récupération ou création d'une souscription auprès d'un push service
+        .then(registration =>{
+        
+        	// tentative d'obtention d'une souscription
+            // public vapid key générée par web-push, en prod appel d'api via fetch plutôt que static
+            const publicKey = "BN4YXZBpAD_ym_CHqvRQXB7UemsopIS06d3VE7DtQE2gzb28q25kgS02S-ijL1vQLTFSFjQngzZK_J7D92a48_8";
+            console.log("publicKey", publicKey);
+            registration.pushManager.getSubscription().then(subscription => {
+            
+            	// Déjà une souscription, on l'affiche
+                if(subscription){
+                    console.log("subscription", subscription);
+                    extractKeysFromArrayBuffer(subscription);
+                    return subscription;
+                }
+                
+                // Pas de souscription
+                else{
+                    // demande de souscription (clef convertie en buffer pour faire la demande)
+                    const convertedKey = urlBase64ToUint8Array(publicKey);
+                    return registration.pushManager.subscribe({
+                        userVisibleOnly: true, // accord de confiance
+                        applicationServerKey: convertedKey
+                    })
+                    .then(newSubscription => {
+                    	// Affiche le résultat pour vérifier
+                        console.log('newSubscription', newSubscription);
+                        extractKeysFromArrayBuffer(newSubscription);
+                        return newSubscription;
+                    })
+
+                }
+            })
+        })
+        .catch(err => console.error('service worker NON enregistré', err));
+}
+
+// 8.4 Récupération ou création d'une souscription auprès d'un push service
+// Fonction pour récupérer les clés de la souscription afin de les utiliser pour notification
+function extractKeysFromArrayBuffer(subscription){
+    // no more keys proprety directly visible on the subscription objet. So you have to use getKey()
+    const keyArrayBuffer = subscription.getKey('p256dh');
+    const authArrayBuffer = subscription.getKey('auth');
+    const p256dh = btoa(String.fromCharCode.apply(null, new Uint8Array(keyArrayBuffer)));
+    const auth = btoa(String.fromCharCode.apply(null, new Uint8Array(authArrayBuffer)));
+    console.log('p256dh key', keyArrayBuffer, p256dh);
+    console.log('auth key', authArrayBuffer, auth);
+    
+    // Paramètres nécessaires à l'objet de notification pushSubscription
+    console.log('endpoint :');
+    console.dir(subscription.endpoint);
+    console.log('p256dh key :', p256dh);
+    console.log('auth key :', auth);
+}
+
+
+// 8.4 Récupération ou création d'une souscription auprès d'un push service
+// Fonction pour convertir string en array buffer pour envoie au push service
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
